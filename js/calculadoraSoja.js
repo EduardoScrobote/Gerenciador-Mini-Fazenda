2c3ee882bdb483dde4c70b311324daad841942c8
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2023-01-19 13:11:55 -0300

    Hoje eu decidi expandir um pouco as posibilidades da minha aplicação, adicionei uma forma do usuario pode escolher o produto e a unidade dele para ser gerado o valor que foi adquirido com a produção.

--- js/calculadoraSoja.js
+++ js/calculadoraSoja.js
@@ -1,16 +1,32 @@
 
-const button = document.getElementById("calculate")
+const button = document.getElementById("calculate");
 
 button.addEventListener("click", () => {
+    const valueDay = Number(document.getElementById("valueDay").value);
+    const quantity = Number(document.getElementById("quantity").value);
+    const product = document.getElementById("product").value;
+    const unity = document.getElementById("unity").value;
     const calcu = document.getElementById("calcu")
-    const value = document.getElementById("value")
-    const quantity = document.getElementById("quantity")
-
-    const exactQuantity = Number(quantity.value) / 60
-    const totalEarn = Number(exactQuantity) * Number(value.value);
-    console.log(totalEarn.toFixed([0]))
-    calcu.classList.add("result")
-return calcu.innerHTML = `<p>Seus ganhos são <span> R$${Number(totalEarn.toFixed([0]))} </span></p>`
-})
-
+    console.log(valueDay, quantity, product, unity)
+    let calculo = (product, unity) => {
+        if (product === "moybean" && unity === "saca") {
+            return calcu.innerHTML += (`<p>O valor total é ${quantity * valueDay}</p>`)
+        }  else if (product === "moybean" && unity === "kg") {
+            return calcu.innerHTML +=(`<p>O valor total é ${quantity / 60 * valueDay}</p>`)
+        }   else if (product === "corn" && unity === "saca") {
+            return calcu.innerHTML +=(`<p>O valor total é ${quantity * valueDay}</p>`)
+        }   else if (product === "corn" && unity === "kg") {
+            return calcu.innerHTML +=(`<p>O valor total é ${quantity / 60  * valueDay}</p>`)
+        }   else if (product === "wheat" && unity === "saca") {
+            return calcu.innerHTML +=(`<p>O valor total é ${quantity * valueDay}</p>`)
+        }   else if (product === "wheat" && unity === "kg") {
+            return calcu.innerHTML +=(`<p>O valor total é ${quantity / 60 * valueDay}</p>`)
+        }   else if (product === "strawberry" && unity === "box") {
+            return calcu.innerHTML +=(`<p>O valor total é ${quantity * valueDay}</p>`)
+        } else {
+           calcu.innerHTML = `<p>Impossivel calcular</p>`
+        }
+    };
+    return console.log(calculo(product, unity))
+});
 
